5e8d7de93670971336bdc65c4119174323adf83b
rachelbookhout <rbookhou@gmail.com>
2014-09-13 16:10:33 -0400

    can delete tasks by deleting tasklist

--- app/models/tasklist.rb
+++ app/models/tasklist.rb
@@ -1,7 +1,7 @@
 class Tasklist < ActiveRecord::Base
 # has_many :tasks, through: :to_do_lists
 # has_many :to_do_lists
-has_many :tasks
+has_many :tasks , dependent: :destroy
 has_many :user_lists
 has_many :users, through: :user_lists
 end
